09155801debf78537dc86a493b70d58afb0ff363
Rojesh Dangol <rxd1026@sru.com>
2019-11-24 14:26:49 -0500

    routing is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './home/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PeopleComponent } from './people/people.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { StarshipsComponent } from './starships/starships.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'people', component: PeopleComponent},
+  { path: 'planets', component: PlanetsComponent},
+  { path: 'starships', component: StarshipsComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { BodyComponent } from './home/body/body.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PeopleComponent } from './people/people.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { StarshipsComponent } from './starships/starships.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    HomeComponent,
+    NotFoundComponent,
+    PeopleComponent,
+    PlanetsComponent,
+    StarshipsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [PeopleComponent, ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
